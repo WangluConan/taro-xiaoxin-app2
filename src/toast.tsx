f0e544e77aba78edb8b906850a0237c800df10e0
WangluConan <2261076785@qq.com>
2023-06-01 16:35:41 +0800

    上传代码

---
+++ src/toast.tsx
@@ -0,0 +1,17 @@
+import { View } from '@tarojs/components';
+import { useState } from 'react';
+import { Toast } from 'tard'
+
+const Toasts = () => {
+    const [toastParams, setToast] = useState({ text: '普通提示', visible: true });
+    return (
+        <View className="container">
+            <Toast
+                {...toastParams}
+                onClose={() => setToast({visible: false, text: '',}) }
+            />
+        </View>
+    );
+};
+
+export default Toasts
